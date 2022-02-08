ddc549aff9d2468369d39c9a0b6b4f33cb581fce
Devendra Singh Shakya <davshakya@gmail.com>
2022-02-08 10:42:29 +0530

    log folder

--- Practice1/resize.py
+++ Practice1/resize.py
@@ -1,32 +1,5 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-
-driver=webdriver.Chrome(executable_path="chromedriver.exe")
-driver.maximize_window()
-driver.get('https://jqueryui.com/resizable/')  
-time.sleep(8)
-
-fr=driver.find_element_by_class_name('demo-frame')
-driver.switch_to_frame(fr)
-# obj=driver.find_element_by_xpath('//*[@id="resizable"]/div[1]')
-obj=driver.find_element_by_xpath("//div[@class='ui-resizable-handle ui-resizable-e']")
-ActionChains(driver).drag_and_drop_by_offset(obj,150,150).perform()
-time.sleep(2)
-driver.close()
-
-
-
-
-
-
-
-
- 
- 
- 
- 
- 
- 
+driver=webdriver.Chrome(executable_path="chromedrive.exe")
+driver.get("http://www.facebook.com")
