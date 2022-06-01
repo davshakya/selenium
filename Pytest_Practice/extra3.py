bf8a8edef38e69caf4d85ac59f820a1694cf95f1
Devendra Singh Shakya <68561473+davshakya@users.noreply.github.com>
2022-06-01 11:41:01 +0530

    hi

---
+++ Pytest_Practice/extra3.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path="C:\work\chromedriver.exe")
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+
+driver.find_element_by_xpath("//input[@id='name']").send_keys("Devendra")
+driver.find_element_by_xpath("//input[@id='alertbtn']").click()
+popup=driver.swithch
+popup.dismiss()
+
+
+time.sleep(3)
+driver.close()
