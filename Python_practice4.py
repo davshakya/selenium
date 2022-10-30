9fe068c6890ecf2693fb1e8184ad6a1f1db2d97f
Siva.Sankar <siva.sankar@x.builder.ai>
2022-10-30 10:52:19 +0530

    hi

---
+++ Python_practice4.py
@@ -0,0 +1,22 @@
+# def outerfun(add):
+#     def inner():
+#         x=add()
+#         y=x+5
+#         print(y)
+#     return inner
+
+
+# @outerfun
+# def add():
+#     a=5
+#     return a
+# add()
+
+
+
+
+
+a=5
+c=a
+z=a+c
+print("Hellow world")
