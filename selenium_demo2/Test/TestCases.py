cdeb915739fbccce28862e81377b4556cdd5ce09
Devendra <davshakya@gmail.com>
2023-03-23 20:53:20 +0530

    pytest framework added

--- selenium_demo2/Test/TestCases.py
+++ selenium_demo2/Test/TestCases.py
@@ -1,62 +1,51 @@
 import time
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from utiles.web_xpaths import WebXpath
+from PageObjects.HomePage import Home
+import pytest
+from Utilities.logger import Logclass
 
-class Test_Ui_Practice:
-    def test_switch_windows(self):
-        driver = webdriver.Chrome(executable_path="c:\work\chromedriver.exe")
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
-        driver.maximize_window()
-        main=driver.window_handles[0]
-        driver.find_element(By.XPATH, WebXpath.open_window).click()
-        w1=driver.window_handles[1]
-        driver.switch_to.window(w1)
-        driver.maximize_window()
-        driver.find_element(By.XPATH, "//div[@id='navbarSupportedContent']//a[contains(text(),'Courses')]").click()
-        time.sleep(5)
-        driver.close()
-        driver.switch_to.window(main)
-        time.sleep(2)
-        driver.find_element(By.XPATH, "//a[@id='opentab']").click()
-        t1=driver.window_handles[1]
-        driver.switch_to.window(t1)
-        driver.find_element(By.XPATH, "//a[contains(text(),'JOIN NOW')]").click()
-        driver.close()
-        time.sleep(5)
-        driver.quite()
+
+@pytest.mark.usefixtures("setup")
+class TestUiPractice(Logclass):
+    def test_set_text(self):
+        log=self.get_logs()
+        hp = Home(self.driver)
+        hp.input_text("Devendra Shakya")
+        log.info("Entered user name")
+        entered_text = self.driver.find_element(By.XPATH, hp.set_text).get_attribute("value")
+        print("entered_text" + "=" + entered_text)
+        assert entered_text == "Devendra Shakya",log.info("#####Test case failed")
+        log.info("test_set_text case passed")
 
     def test_checkboxes(self):
-        driver = webdriver.Chrome(executable_path="c:\work\chromedriver.exe")
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
-        driver.maximize_window()
-        driver.find_element(By.XPATH, WebXpath.checkbox1).click()
-        driver.find_element(By.XPATH, WebXpath.checkbox2).click()
-        driver.find_element(By.XPATH, WebXpath.checkbox3).click()
+        log=self.get_logs()
+        hp = Home(self.driver)
+        self.driver.find_element(By.XPATH, hp.checkbox1).click()
+        self.driver.find_element(By.XPATH, hp.checkbox2).click()
+        self.driver.find_element(By.XPATH, hp.checkbox3).click()
+        is_checkbox_enabled = self.driver.find_element(By.XPATH, hp.checkbox3).is_selected()
+        assert is_checkbox_enabled == True
+        log.info("test_checkboxes case passed")
+
+    def test_switch_windows(self):
+        log=self.get_logs()
+        hp = Home(self.driver)
+        main = self.driver.window_handles[0]
+        current_title = self.driver.title
+        print(current_title)
+        assert current_title == "Practice Page"
+        self.driver.find_element(By.XPATH, hp.open_window).click()
+        w1 = self.driver.window_handles[1]
+        self.driver.switch_to.window(w1)
+        self.driver.maximize_window()
+        self.driver.find_element(By.XPATH, "//div[@id='navbarSupportedContent']//a[contains(text(),'Courses')]").click()
         time.sleep(5)
-        driver.quite()
-
-
-    def test_set_text(self):
-        driver = webdriver.Chrome(executable_path="c:\work\chromedriver.exe")
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
-        driver.maximize_window()
-        driver.find_element(By.XPATH, WebXpath.set_text).send_keys("Devendra Shakya")
-        time.sleep(5)
-        driver.quite()
-
-
-    def test_set_dropdown_option(self):
-        driver = webdriver.Chrome(executable_path="c:\work\chromedriver.exe")
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
-        driver.maximize_window()
-        driver.find_element(By.XPATH, WebXpath.dropdown).click()
-        driver.find_element(By.XPATH, WebXpath.dropdown_option1).click()
+        self.driver.close()
+        self.driver.switch_to.window(main)
         time.sleep(2)
-        driver.find_element(By.XPATH, WebXpath.dropdown).click()
-        driver.find_element(By.XPATH, WebXpath.dropdown_option2).click()
-        time.sleep(2)
-        driver.find_element(By.XPATH, WebXpath.dropdown).click()
-        driver.find_element(By.XPATH, WebXpath.dropdown_option3).click()
-        time.sleep(5)
-        driver.quite()
+        self.driver.find_element(By.XPATH, "//a[@id='opentab']").click()
+        t1 = self.driver.window_handles[1]
+        self.driver.switch_to.window(t1)
+        self.driver.find_element(By.XPATH, "//a[contains(text(),'JOIN NOW')]").click()
+        self.driver.close()
+        log.info("test_switch_windows case passed")
