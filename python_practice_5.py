5e8a9c4011cffb080f2184be0263eb1dc6d5825d
Devendra <davshakya@gmail.com>
2023-06-13 08:31:53 +0530

    hi

--- python_practice_5.py
+++ python_practice_5.py
@@ -100,4 +100,191 @@ import re
 
 
 
+# data_list = [5, 0, 23, -6, 23, 67]
+# max=[0]
+#
+# for i in range(len(data_list)):
+#     if max[0]<data_list[i]:
+#         max[0]=data_list[i]
+# print(max)
+
+#
+# def outer(add):
+#     def inner():
+#         b=add()**2
+#         print(b)
+#     return inner()
+#
+# @outer
+# def add():
+#     a=5
+#     return a
+#
+#
+# import copy
+# #
+# old_list = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+# new_list = copy.copy(old_list)
+#
+# old_list[1][1] = 'AA'
+# print("Old list:", old_list)
+# print("New list:", new_list)
+# #
+# #
+# old_list1 = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+# new_list1 = copy.deepcopy(old_list1)
+#
+# old_list1[1][0] = 'BB'
+# print("Old list1:", old_list1)
+# print("New list1:", new_list1)
+
+
+# number = 123321
+# s = str(number)
+# rs = ""
+# for i in range(len(list(s))):
+#     rs = s[i]+rs
+# if s == rs:
+#     print("It is polynomial")
+# else:
+#     print("it is not polynomial")
+
+
+
+
+# num=123
+# rev=0
+# while num>0:
+#     rem=num%10
+#     rev=rev*10+rem
+#     num=num//10
+# print(rev)
+
+# n=12345
+# while n>0:
+#     n=n//10
+#     print(n)
+
+#
+# for i in range(1,5):
+#     for j in range(i-1,i):
+#         print(str(i)*i)
+
+# row=3
+# col=5
+# for i in range(row):
+#     for j in range(i):
+#         print("*"*col)
+
+#
+# def creating_gen(index):
+#     months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+#     yield months[index]
+#     yield months[index+2]
+# print(next(creating_gen(2)))
+
+
+# s="deve"
+# print(dir(s))
+
+
+# import time
+# s_time=time.time()
+# s="cab413"
+# ls=list(s)
+# l=[]
+# for i in ls:
+#     l=[x for x in l if x<i]+[i]+[x for x in l if x>=i]
+# e_time=time.time()
+# print("".join(l))
+# print(e_time-s_time)
+
+
+# s_time=time.time()
+# s="cab413"
+# ls=list(s)
+# for i in range(len(ls)):
+#     for j in range(i):
+#         if ls[i]<ls[j]:
+#             ls[i], ls[j] = ls[j], ls[i]
+# e_time=time.time()
+# print(ls)
+# print(e_time-s_time)
+
+# l=[12,3,4,2,31]
+# # print(sorted(l))
+# l.sort()
+# l.reverse()
+# print(l)
+
+# s="deves ha"
+# k=s.split()
+# print(k)
+
+# s="man can san man can tan man tan"
+# d={x:s.count(x) for x in s.split()}
+# for keys, values in d.items():
+#     if values>=2:
+#         print(keys,values)
+# # print(d)
+
+
+# s="a3b2c4"
+# ls=list(s)
+# ns=""
+# for i in ls:
+#     if i.isalpha():
+#         var=i
+#     else:
+#         ns=ns+var*int(i)
+# print(ns)
+
+# import sys
+# print(sys.getsizeof("d"))
+
+# print(ord('a'))
+# print(ord("1"))
+# print(chr(100))
+# print(id('abc'))
+# print(id('abc'))
+
+# l=[12,2,34,4]
+# l.reverse()
+# print(l)
+
+# import openpyxl
+# wb=openpyxl.load_workbook("sample.xlsx")
+# sht=wb["Sheet1"]
+# cl=sht.cell(1,3).value
+# for i in range(2,5):
+#     print(sht.cell(i,3).value)
+
+# import xlrd
+# wb=xlrd.open_workbook("sample.xlsx")
+# sheet=wb.sheet_by_index(0)
+# cell=sheet.cell_value(2,2)
+# print(cell)
+
+
+
+# f=open("test.txt", "r+")
+# f.write("Hello shakya manan")
+# # p=f.read()
+# p=f.readlines()
+# print(p)
+# f.close()
+
+
+
+
+###Reverse line of text###########
+s="This my pen which is not your"
+sl=s.split()
+l=[]
+for i in sl:
+    k=""
+    for j in range(len(list(i))):
+        k=i[j]+k
+    l.append(k) 
+print(" ".join(l)) 
 
