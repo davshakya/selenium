6fc34f4737c969f03b1e759cc4f4deb39b734004
Devendra Singh Shakya <davshakya@gmail.com>
2023-08-10 08:23:59 +0530

    hi

--- Practice1/Explicit_wait.py
+++ Practice1/Explicit_wait.py
@@ -8,15 +8,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-driver = webdriver.Chrome(service=Service("C:\work\chromedriver.exe"))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver")
 driver.maximize_window()
 WebDriverWait(driver,11).until(expected_conditions.element_to_be_clickable((By.XPATH,"//body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]")))
 # driver.find_element_by_xpath("//body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]").click()
 driver.find_element(By.XPATH,"//button[@id='populate-text']").click()
 
-WebDriverWait(driver, 15).until(
-    expected_conditions.text_to_be_present_in_element((By.XPATH, "//h2[@id='h2']"), "Selenium Webdriver"))
+WebDriverWait(driver, 15).until(expected_conditions.text_to_be_present_in_element((By.XPATH, "//h2[@id='h2']"), "Selenium Webdriver"))
 print(driver.find_element(By.XPATH,"//h2[@id='h2']").text)
 time.sleep(2)
 driver.close()
